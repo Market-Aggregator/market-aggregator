d1e5a34a24266a938dd58674ac8a976e0645c3af
Aliff Amir <aliff6406@gmail.com>
2025-07-28 14:33:27 +0100

    add StockTrade entity

---
+++ MarketOverviewService/MarketOverviewService.Core/Entities/StockTrade.cs
@@ -0,0 +1,11 @@
+namespace MarketOverviewService.Core.Entities;
+
+public class StockTrade
+{
+    public int StockTradeId {get;set;}
+    public required string Symbol {get;set;}
+    public required string Exchange {get;set;}
+    public decimal Price {get;set;}
+    public int Size {get;set;}
+    public DateTimeOffset Timestamp {get;set;}
+};
