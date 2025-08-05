8ec1d9f1ce467c7149da90cd8557751a5ae1a311
Aliff Amir <aliff6406@gmail.com>
2025-08-05 22:01:52 +0100

    remove worker project

--- MarketOverviewService/MarketOverviewService.Core/Interfaces/ILiveMarketDataConsumer.cs
+++
@@ -1,7 +0,0 @@
-using MarketOverviewService.Core.Entities;
-
-namespace MarketOverviewService.Core.Interfaces;
-
-public interface ILiveMarketDataConsumer {
-    Task<StockTrade?> ConsumeAsync(CancellationToken ct);
-}
