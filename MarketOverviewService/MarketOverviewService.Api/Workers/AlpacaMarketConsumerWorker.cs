2118f09c47571e655b1205573e9417cedd4a147f
Aliff Amir <aliff6406@gmail.com>
2025-08-18 20:55:10 +0100

    persist stock trade on consumption

--- MarketOverviewService/MarketOverviewService.Api/Workers/AlpacaMarketConsumerWorker.cs
+++ MarketOverviewService/MarketOverviewService.Api/Workers/AlpacaMarketConsumerWorker.cs
@@ -7,12 +7,19 @@ public class AlpacaMarketConsumerWorker : BackgroundService
     private readonly IPublisher _publisher;
     private readonly ILogger<AlpacaMarketConsumerWorker> _logger;
     private readonly IMarketDataConsumer _marketDataConsumer;
+    private readonly IStockTradeRepository _stockTradeRepo;
 
-    public AlpacaMarketConsumerWorker(IPublisher publisher, IMarketDataConsumer marketDataConsumer, ILogger<AlpacaMarketConsumerWorker> logger)
+    public AlpacaMarketConsumerWorker(
+            IPublisher publisher,
+            IMarketDataConsumer marketDataConsumer,
+            ILogger<AlpacaMarketConsumerWorker> logger,
+            IStockTradeRepository stockTradeRepo
+            )
     {
         _publisher = publisher;
         _marketDataConsumer = marketDataConsumer;
         _logger = logger;
+        _stockTradeRepo = stockTradeRepo;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,9 +29,13 @@ public class AlpacaMarketConsumerWorker : BackgroundService
             _logger.LogInformation("AlpacaMarket Consumer Worker running at: {time}", DateTimeOffset.Now);
         }
 
-        await foreach (var trade in _marketDataConsumer.ConsumeAsync("N", stoppingToken))
+        await foreach (var trade in _marketDataConsumer.ConsumeAsync("V", stoppingToken))
         {
             await _publisher.BroadcastTradeAsync(trade);
+
+            // TODO: enqueue to in-memory queue (using channels) to persist asynchronously
+            // so that we don't block streaming of trades to clients
+            await _stockTradeRepo.CreateAsync(trade);
         }
     }
 }
