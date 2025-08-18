318c30fa24ce2c8273ff88612736addab3f1f1cf
Aliff Amir <aliff6406@gmail.com>
2025-08-18 21:53:55 +0100

    add AlpacaTestMarketWorker as hosted service

--- MarketFeedService/MarketFeedService.Worker/Program.cs
+++ MarketFeedService/MarketFeedService.Worker/Program.cs
@@ -5,6 +5,7 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddHostedService<AlpacaMarketWorker>();
+builder.Services.AddHostedService<AlpacaTestMarketWorker>();
 
 var host = builder.Build();
 host.Run();
